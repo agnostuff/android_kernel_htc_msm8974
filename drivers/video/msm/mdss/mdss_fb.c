64eb565eb669a988e7d1b3ae7121a2661a918884
agnostuff <agnostuff@gmail.com>
2020-03-24 13:31:05 +0100

    patch for refresh rate PMOS
    @@ -1717,1 +1717,1 @@ static int mdss_fb_register(struct msm_fb_data_type *mfd)
    -	var->pixclock = panel_info->clk_rate / 1000;
    +	var->pixclock = 1000000 / 60;

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -1714,7 +1714,7 @@ static int mdss_fb_register(struct msm_fb_data_type *mfd)
 	var->left_margin = panel_info->lcdc.h_back_porch;
 	var->right_margin = panel_info->lcdc.h_front_porch;
 	var->hsync_len = panel_info->lcdc.h_pulse_width;
-	var->pixclock = panel_info->clk_rate / 1000;
+	var->pixclock = 1000000 / 60;
 
 	if (panel_info->camera_blk || panel_info->camera_dua_blk) {
 		htc_register_camera_bkl(panel_info->camera_blk, panel_info->camera_dua_blk);
